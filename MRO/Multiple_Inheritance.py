2787f6188c06eb8da8e93b3d40187bbf891be3c7
SQ <12221112@zju.edu.cn>
2024-11-04 14:57:59 +0000

    添加多重继承示例，展示MRO顺序及方法调用

---
+++ MRO/Multiple_Inheritance.py
@@ -0,0 +1,30 @@
+class A:
+    def __init__(self, *args, **kwargs):
+        print("A's __init__")
+        super().__init__(*args, **kwargs)
+    
+    def say_hello(self):
+        print("Hello from A")
+
+class B:
+    def __init__(self, *args, **kwargs):
+        print("B's __init__")
+        super().__init__(*args, **kwargs)
+        
+    def say_hello(self):
+        print("Hello from B")
+
+class C(A, B):
+    def __init__(self, *args, **kwargs):
+        print("C's __init__")
+        super().__init__(*args, **kwargs)
+
+# 创建 C 的实例
+c = C()
+
+'''
+当子类调用一个重名的函数时，Python 会按照 MRO 的顺序查找并调用第一个匹配的函数。
+你可以通过 ClassName.__mro__ 属性查看类的 MRO。
+'''
+
+c.say_hello()
