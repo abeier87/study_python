1e9c994da98f8d83ee5810550605dd942f6a1c1b
SQ <12221112@zju.edu.cn>
2024-11-07 14:57:01 +0000

    重构多重继承示例，删除旧文件并添加新案例以展示类的初始化和方法调用

---
+++ MRO/Multiple_Inheritance_5.py
@@ -0,0 +1,26 @@
+class grandpa:
+    def __init__(self):
+        print("a")
+    
+    def say_hello(self):
+        print("hello from grandpa")
+
+
+class f_1(grandpa):
+    def __init__(self):
+        print("f1")
+
+class f_2(grandpa):
+    def __init__(self):
+        print("f2")
+    
+    def say_hello(self):
+        print("hello from f2")
+    
+
+class son(f_1, f_2):
+    def __init__(self):
+        print("son")
+        
+son_1 = son()
+son_1.say_hello()
